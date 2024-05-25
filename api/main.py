0864d5bbee3cfbb57ca4637e8e385a1fd192fe99
rozoomcool <rozoom.um@gmail.com>
2024-05-25 17:27:04 +0300

    .

--- api/main.py
+++ api/main.py
@@ -9,6 +9,11 @@ from fastapi import FastAPI, Depends, UploadFile, HTTPException, File, Body, For
 from motor import motor_asyncio
 from pydantic import BaseModel
 from bson import ObjectId
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
 
 MONGO_DETAILS = "mongodb://mongo:27017/bot"
 
@@ -16,10 +21,28 @@ client = motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.bot
 order_collection = database.get_collection("order_collection")
 
+
+class LimitUploadSize(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
+        super().__init__(app)
+        self.max_upload_size = max_upload_size
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        if request.method == 'POST':
+            if 'content-length' not in request.headers:
+                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED)
+            content_length = int(request.headers['content-length'])
+            if content_length > self.max_upload_size:
+                return Response(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+        return await call_next(request)
+
+
 app = FastAPI(
     upload_max_size="100MB",
     max_request_size=1024 * 1024 * 1024
 )
+
+app.add_middleware(LimitUploadSize, max_upload_size=100_000_000)
 
 
 class Item(BaseModel):
