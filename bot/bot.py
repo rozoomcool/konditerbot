338cdbbeae7761090d770c69670cfe43d6f3b03f
rozoomcool <rozoom.um@gmail.com>
2024-05-25 16:48:18 +0300

    .

--- bot/bot.py
+++ bot/bot.py
@@ -14,7 +14,8 @@ from aiogram.types import Message, InputFile, BufferedInputFile, InputMediaPhoto
 from aiogram.utils.payload import decode_payload
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from motor import motor_asyncio
-
+from motor.core import AgnosticCollection
+from motor.motor_asyncio import AsyncIOMotorCollection
 
 MONGO_DETAILS = "mongodb://mongo:27017/bot"
 # MONGO_DETAILS = "mongodb://localhost:27017"
@@ -23,8 +24,8 @@ client = motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
 database = client.bot
 
-user_collection = database.get_collection("users_collection")
-order_collection = database.get_collection("order_collection")
+user_collection: AgnosticCollection = database.get_collection("users_collection")
+order_collection: AgnosticCollection = database.get_collection("order_collection")
 
 
 TOKEN = "6840739601:AAEM6oMDbD7FqO9LsKdMZzn7tXhSeUQU3Ns"
@@ -60,7 +61,7 @@ def order_to_text(order: Dict) -> str:
 
 async def send_orders():
     try:
-        orders = await order_collection.find({})
+        orders = order_collection.find({})
         print(f"orders: {orders}")
         for order in orders:
             order_text = order_to_text(order)
